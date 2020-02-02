1eda89ab70616e6a4c08c046953812cc464dc47b
halilski1979 <halilski1979@gmail.com>
2020-02-02 13:34:48 +0200

    m2

---
+++ Modul2_03/05_Word_2_CeliChisla/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Word_2_CeliChisla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                //Напишете програма, която присвоява цели стойности на променливи. Уверете се, че всяка стойност е записана в правилния тип (във всеки случай използвайте възможно най-икономичния тип по отношение на паметта). Накрая изведете всички променливи в конзолата
+                sbyte num1 = -100;
+                byte num2 = 128;
+                short num3 = -3540;
+                ushort num4 = 64876;
+                uint num5 = 2147483648;
+                int num6 = -1141583228;
+                long num7 = -1223372036854775808;
+
+                Console.WriteLine(num1);
+                Console.WriteLine(num2);
+                Console.WriteLine(num3);
+                Console.WriteLine(num4);
+                Console.WriteLine(num5);
+                Console.WriteLine(num6);
+                Console.WriteLine(num7);
+            }
+        }
+    }
+}
