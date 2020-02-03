47d887445dc6676829f1140687857ce6244ef88b
halilski1979 <halilski1979@gmail.com>
2020-02-03 13:42:44 +0200

    m

---
+++ Modul2_03/07_Pr_1_SpecialniChisla/07_Pr_1_SpecialniChisla/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_Pr_1_SpecialniChisla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("n=");
+            int n = int.Parse(Console.ReadLine());
+            int ost = 0;
+            int s=0;
+            
+
+            for (int i = 0; i < n; i++)
+            {
+                s = 0;
+                int chislo = i;
+                while (chislo>0)
+                {
+                  
+                    ost = chislo % 10;
+                    chislo = chislo / 10;
+                    s = s + ost;
+
+                }
+
+                bool flag = (s == 5 || s == 7 || s == 11);
+                Console.WriteLine($"{flag} ");
+            }
+        }
+    }
+}
---
+++ Modul2_03/07_Word_3_BulPr/07_Word_3_BulPr/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_Word_3_BulPr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n = Console.ReadLine();
+            Convert.ToBoolean(n);
+            if (n=="true")
+            {
+                Console.WriteLine("Yes");
+            }
+            else if(n=="false")
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
