6026ff5725ed210b1ef500054ac1c4d28ad043be
halilski1979 <halilski1979@gmail.com>
2020-02-08 19:01:38 +0200

    m

---
+++ Modul2_04_Masivi/10_Word_3_IndexBukva/10_Word_3_IndexBukva/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_Word_3_IndexBukva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] alphabet = new char[30];
+            int index = 1;
+            for (char i = 'a'; i <= 'z'; i++)
+            {
+
+                alphabet[index] = i;
+                index++;
+            }
+
+            Console.WriteLine(String.Join(" ",alphabet));
+
+            string word = Console.ReadLine();
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                for (int j = 0; j < alphabet.Length; j++)
+                {
+                    if (word[i]==alphabet[j])
+                    {
+                        Console.WriteLine($"{word[i]}->{j}");
+                    }
+                }
+            }
+        }
+
+       
+
+       
+    }
+}
---
+++ Modul2_04_Masivi/Obshti_Upr/Obshti_Upr/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obshti_Upr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = new int [10];
+        }
+    }
+}
