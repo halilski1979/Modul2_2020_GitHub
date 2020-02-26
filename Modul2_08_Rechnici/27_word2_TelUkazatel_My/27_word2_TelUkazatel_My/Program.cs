297d9c74afc6609b0ae370c7c1ce3f793a054d88
halilski1979 <halilski1979@gmail.com>
2020-02-26 21:00:56 +0200

    n

---
+++ Modul2_08_Rechnici/27_word2_TelUkazatel_My/27_word2_TelUkazatel_My/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27_word2_TelUkazatel_My
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> ukazatel = new Dictionary<string, string>();
+            List<string> linia = Console.ReadLine().Split(' ').ToList();
+            while (true)
+            {
+                
+                var command = linia[0];
+                if (command=="end")
+                {
+                    break;
+                }
+
+                
+
+                //Add and update - OK
+                if (command=="A")
+                {
+                    var name = linia[1];
+                    var nomer = linia[2];
+                    if (ukazatel.ContainsKey(name))
+                    {
+                        ukazatel.Remove(name);
+                        name = linia[1];
+                        nomer = linia[2];
+                    }
+                    ukazatel.Add(name,nomer);
+                    Console.WriteLine(string.Join(" ", ukazatel));
+                }
+
+                //Print 
+                if (command == "S")
+                {
+                    string name = linia[1];
+                   
+                        if (ukazatel.ContainsKey(name))
+                        {
+
+                            Console.WriteLine($"Ime:{name}=>Nomer: {ukazatel[name]}");
+                        }
+                        else
+                        {
+                            Console.WriteLine($" {name} does not exist");
+                        }
+                    
+                }
+                linia = Console.ReadLine().Split(' ').ToList();
+            }
+            
+        }
+    }
+}
---
+++ Modul2_08_Rechnici/27_word_3_MinyoskaZadacha_MY/27_word_3_MinyoskaZadacha_MY/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27_word_3_MinyoskaZadacha_MY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> polIzk = new Dictionary<string, int>();
+
+            while (true)
+            {
+                Console.Write("Resource OR stop:");
+                string resource = Console.ReadLine();
+                if (resource=="stop")
+                {
+                    break;
+                }
+
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (polIzk.ContainsKey(resource))
+                {
+                    polIzk[resource] += quantity;
+                    
+                }
+                else
+                {
+                    polIzk.Add(resource, quantity);
+                }
+               
+                    
+                
+                
+            }
+            Console.WriteLine(string.Join(" ",polIzk ));
+        }
+    }
+}
