ef0fe4c71f509c52ed362f27ce326adfdc00e59d
halilski1979 <halilski1979@gmail.com>
2020-02-26 21:47:20 +0200

    n

---
+++ Modul2_08_Rechnici/27_word_4_Market/27_word_4_Market/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27_word_4_Market
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<double>> market = new Dictionary<string, List<double>>();
+
+            
+            double s = 0;
+            while (true)
+            {
+                List<string> linia = Console.ReadLine().Split(' ').ToList();
+                string stoka = linia[0];
+                if (stoka=="stocked")
+                {
+                    break;
+                }
+
+                double price = double.Parse(linia[1]);
+                double quantity = double.Parse(linia[2]);
+
+                if (market.ContainsKey(stoka))
+                {
+                    //обхождаме речника 
+                    for (int i = 0; i < market[stoka].Count; i++)
+
+                    {
+
+                        if (market[stoka][0] != price)
+
+                        {
+
+                            market[stoka][0] = price;
+
+                        }
+
+                        market[stoka][1] += quantity;
+
+                    }
+                }
+                else
+                {
+                    var list = new List<double>();
+                    list.Add(price);
+                    list.Add(quantity);
+                    market.Add(stoka, list);
+                }
+                
+
+            }
+
+            foreach (var item in market)
+            {
+                Console.WriteLine($"{item.Key}: ${item.Value[0]:f2}*{item.Value[1]} = ${item.Value[0]*item.Value[1]:f2}");
+                s += item.Value[0] * item.Value[1];
+            }
+            Console.WriteLine("----------------------");
+            Console.WriteLine($"Grand total: ${s:f2}");
+        }
+    }
+}
